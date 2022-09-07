27cbcad16cfe51a261b988ad74907e9a284ef246
Javan <javanlevey@gmail.com>
2022-09-07 14:07:52 -0400

    Started Front End

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Header() {
+    return (
+        <header className='App-header'>
+            <img src='public/logo512.png'></img>
+            <h1>Triple J Brewery</h1>
+
+        </header>
+    )
+}
