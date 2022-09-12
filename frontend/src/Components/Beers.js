fb8af25047354c9167e73aa0a8b701c999cba18b
Javan <javanlevey@gmail.com>
2022-09-12 00:05:30 -0400

    Still a long way to go

--- frontend/src/Components/Beers.js
+++ frontend/src/Components/Beers.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {addBeer, deleteBeer, viewBeers} from './Crud'
 
 // export default function Beers {
 //     return {
---
+++ frontend/src/Components/Crud.js
@@ -0,0 +1,27 @@
+// const url = '';
+
+// export function addBeer() {
+
+// }
+
+// export function deleteBeer() {
+
+// }
+
+// export function viewBeers() {
+
+// }
+
+// export function newBrewery() {
+
+// }
+
+// export function updateBrewery() {
+
+// }
+
+// export function viewBreweries() {
+
+// }
+
+
